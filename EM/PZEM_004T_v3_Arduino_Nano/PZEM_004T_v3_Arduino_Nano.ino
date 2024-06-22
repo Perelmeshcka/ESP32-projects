ed644887bd808ba414763dca440e98d3a47f2114
Yar Sys <yyariyyarikk@gmail.com>
2024-06-22 20:37:31 +0300

    Restoring ready files (first commit)

--- EM/PZEM_004T_v3_Arduino_Nano/PZEM_004T_v3_Arduino_Nano.ino
+++ EM/PZEM_004T_v3_Arduino_Nano/PZEM_004T_v3_Arduino_Nano.ino
@@ -12,28 +12,28 @@ void setup()
 void loop() 
 {
   float voltage = pzem.voltage();
-  if(voltage != NAN) {
+  if (voltage != NAN) {
     srn::print("= VOLTAGE: " + sif::fill(String(voltage), 6, " ", true) + "V =", 0, true);
   } else {
     srn::print("== VOLTAGE: ERROR ==", 0, true);
   }
 
   float current = pzem.current();
-  if(current != NAN) {
+  if (current != NAN) {
     srn::print("= CURRENT: " + sif::fill(String(current), 6, " ", true) + "A =", 1, true);
   } else {
     srn::print("== CURRENT: ERROR ==", 1, true);
   }
 
   float power = pzem.power();
-  if(power != NAN) {
+  if (power != NAN) {
     srn::print("= POWER: " + sif::fill(String(power), 8, " ", true) + "W =", 2, true);
   } else {
     srn::print("=== POWER: ERROR ===", 2, true);
   }
 
   float energy = pzem.energy();
-  if(energy != NAN) {
+  if (energy != NAN) {
     srn::print("= ENERGY: " + sif::fill(String(power), 5, " ", true) + "kWh =", 3, true);
   } else {
     srn::print("== ENERGY:  ERROR ==", 3, true);

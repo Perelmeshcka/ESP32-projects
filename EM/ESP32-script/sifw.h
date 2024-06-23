88fd6d53a32fa776fb4042d7dddce0383ec22417
Yar Sys <yyariyyarikk@gmail.com>
2024-06-23 12:33:37 +0300

    RTC testing

--- EM/ESP32-script/sifw.h
+++
@@ -1,9 +0,0 @@
-namespace sif {
-    String fill(String text, int size, String filler, bool begin = false) {
-        while (text.length() < size) {
-            if (begin) text = filler + text;
-            else text = text + filler;
-        }
-        return text;
-    }
-};

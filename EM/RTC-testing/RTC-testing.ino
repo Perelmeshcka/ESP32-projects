b9ec464b908d40a8cd19416fb4044612e3c6f79f
Yar Sys <yyariyyarikk@gmail.com>
2024-06-23 14:07:15 +0300

    RTC testing

--- EM/RTC-testing/RTC-testing.ino
+++ EM/RTC-testing/RTC-testing.ino
@@ -2,17 +2,21 @@
 #include <DS3231.h>
 
 DS3231 myRTC;
+RTCDateTime datetime;
 
 void setup()
 {
     Serial.begin(9600);
+    Wire.begin();
+    myRTC.begin();
+    myRTC.enableOutput(false);
+    myRTC.setDateTime(__DATE__, __TIME__);
 }
 
 void loop()
 {
-    Serial.print("Current date: ");
-    Serial.println(F(__DATE__));
-    Serial.print("Current time: ");
-    Serial.println(F(__TIME__));
+    datetime = myRTC.getDateTime();
+    Serial.print("Current DateTime: ");
+    Serial.println(myRTC.dateFormat("d.m.Y H:i:s - l   ", datetime));
     delay(2000);
 }

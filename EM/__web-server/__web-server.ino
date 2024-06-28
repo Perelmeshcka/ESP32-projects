b5fad969f1fe43b161dd6ae9815104f9c8868aa3
Yar Sys <yyariyyarikk@gmail.com>
2024-06-28 18:31:00 +0300

    WEB INTERFACE!!!

--- EM/__web-server/__web-server.ino
+++ EM/__web-server/__web-server.ino
@@ -1,103 +1,67 @@
+#include <Arduino.h>
+#define WIFI_SSID "DVA"
+#define WIFI_PASS "Sysoev_78"
+
+#ifdef ESP8266
+#include <ESP8266WiFi.h>
+#else
 #include <WiFi.h>
-#include <FS.h>
-#include <SPIFFS.h>
-#include "screen.h"
-#include "sifw.h"
+#endif
 
-const char* ssid = "DVA";
-const char* password = "Sysoev_78";
-const char* ip = "192.168.1.12";
-String header = "";
+#include <GyverHTTP.h>
+ghttp::Server<WiFiServer, WiFiClient> server(80);
 
-WiFiServer server(80);
+// файл из релиза, содержит приложение в бинарном gzip виде
+#include "bitmaper.h"
+// #include "page.h"
 
-void sendXMLFile(WiFiClient clt)
-{
-    clt.println("<?xml version = \"1.0\" ?>");
-    clt.println("<inputs>");
+void setup() {
+    Serial.begin(115200);
 
-    clt.print("<reading>");
-    clt.print("value");
-    clt.println("</reading>");
-
-    clt.println("</inputs>");
-}
-
-void setup()
-{
-    srn::SETUP();
-    srn::print("= NW: " + sif::fill(ssid, 12, " ", true) + " =", 0, true);
-    WiFi.begin(ssid, password);
-
-    while (WiFi.status() != WL_CONNECTED) {
-        srn::print("[" + sif::fill(dot::loadingLine(18), 18, "-", false) + "]", 1, false);
-        delay(100);
-    }
-
-    dot::resetLine();
-    srn::print("[##################]", 1, false);
-    delay(500);
-    srn::print(">> WIFI CONNECTED <<", 2, false);
-    delay(500);
-    srn::print("IP: " + sif::fill(ip, 16, " ", true), 3, true);
-
-    // SPIFFS.format();
-    server.begin();
-}
-
-void loop()
-{
-    WiFiClient client = server.available();
-
-    if (client)
-    {
-        bool currentLineIsBlank = true;
-        while (client.connected())
-        {
-            if (client.available())
-            {
-                char c = client.read();
-                header += c;
-
-                if (c == '\n' && currentLineIsBlank)
-                {
-                    client.println("HTTP/1.1 200 OK");
-
-                    if (header.indexOf("update_readings") >= 0)
-                    {
-                        client.println("Content-Type: text/xml");
-                        client.println("Connection: keep-alive");
-                        client.println();
-
-                        sendXMLFile(client);
-                    }
-
-                    else
-                    {
-                        client.println("Content-Type: text/html");
-                        client.println("Connection: keep-alive");
-                        client.println();
-
-                        // SPIFFS.begin();
-                        // File webFile = SPIFFS.open("/index.html", "r");
-                        if (webFile)
-                        {
-                            while (webFile.available()) client.write(webFile.read());
-                            webFile.close();
-                        }
-                    }
-                    break;
-                }
-
-                if (c == '\n')
-                    currentLineIsBlank = true;
-                else if (c != '\r')
-                    currentLineIsBlank = false;
-            }
+    // STA
+    WiFi.mode(WIFI_AP_STA);
+    if (strlen(WIFI_SSID)) {
+        WiFi.begin(WIFI_SSID, WIFI_PASS);
+        uint8_t tries = 20;
+        while (WiFi.status() != WL_CONNECTED) {
+            delay(500);
+            Serial.print(".");
+            if (!--tries) break;
         }
-
-        header = "";
-        client.stop();
-        srn::print("CLIENT DISCONNECTED.", 2, true);
+        Serial.print("Connected: ");
+        Serial.println(WiFi.localIP());
     }
+
+    // AP
+    WiFi.softAP("AP ESP");
+    Serial.print("AP: ");
+    Serial.println(WiFi.softAPIP());
+
+    // server
+    server.begin();
+    server.onRequest([](ghttp::ServerBase::Request req) {
+        switch (req.path().hash()) {
+            case su::SH("/"):
+                server.sendFile_P(bitmaper_index, sizeof(bitmaper_index), "text/html", false, true);
+                break;
+
+            case su::SH("/script.js"):
+                server.sendFile_P(bitmaper_script, sizeof(bitmaper_script), "text/javascript", true, true);
+                break;
+
+            case su::SH("/style.css"):
+                server.sendFile_P(bitmaper_style, sizeof(bitmaper_style), "text/css", true, true);
+                break;
+
+            default:
+                server.send(200);
+                break;
+        }
+    });
+}
+
+void loop() {
+    server.tick();
+    Serial.print("Connected: ");
+    Serial.println(WiFi.localIP());
 }
---
+++ EM/__web-server/page.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <Arduino.h>
+
+namespace web {
+    const uint8_t page_index[] = "<!DOCTYPE html><html lang=\"en\"><head><meta charset=\"UTF-8\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"><title>Hello</title></head><body><h1>HELLO MAN</h1></body></html>";
+    const uint8_t page_style[] = "";
+    const uint8_t page_script[] = "";
+};
